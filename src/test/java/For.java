a05a2e8b9135f18f502fb492ca3fa583a1300865
Сергей <miusov94@bk.ru>
2020-05-03 14:40:13 +0300

    Задания для новичков

---
+++ src/test/java/For.java
@@ -0,0 +1,23 @@
+public class For {
+
+
+    public static void main(String[] args) {
+        String[] names = new String[] { "Маша", "Саша", "Даша", "Серёжа","Пашок"};
+//        for (int i = 1; i < 4; i++) {
+//            if (names[i] == "Даша") {
+//                continue;
+//            }
+//
+//            System.out.println("Имя № " + i + " " + names[i]);
+//        }
+        for (String name : names) {
+            if(name == "Даша") {
+                break;
+            }
+            System.out.println("Имя № " + name);
+        }
+    }
+}
+
+
+
