1dc4710e7084bfc455b0ef62f6f3101deb4f2621
GusevVrn <64732451+GusevVrn@users.noreply.github.com>
2020-05-03 19:42:16 +0300

    Task with doors completed

--- src/test/java/Methods.java
+++ src/test/java/Methods.java
@@ -1,30 +1,58 @@
+// Задание 1: Добавить ещё 10 дверей к существующим
+// Задание 2: Убрать 5 дверей
+// Задание 3: Сделать отдельный метод для вывода в консоль колличества дверей
+// Задание 4*: Сделать отдельный метод в котором вы добавляете по 1 двери в цикле и выводите на консоль
+// Пройти по циклу надо 5 раз
+// Попробовать использовать уже существующие методы в новом
+
 public class Methods {
     public static void main(String[] args) {
-        int doorsCount = 5;
-        System.out.println("Колличество дверей: " + doorsCount);
+        int doorsCount = 0;
 
+// Добавляем дверь
         doorsCount = addOneDoor(doorsCount);
-        System.out.println("Колличество дверей: " + doorsCount);
+// Выводим двери
+        printNumDoors(doorsCount);
 
-        doorsCount = addTwoDoors(doorsCount);
-        System.out.println("Колличество дверей: " + doorsCount);
+// Добавляем 10 дверей
+        for(int i=0;i<10;i++){
+            doorsCount = addOneDoor(doorsCount);
+        }
+// Выводим двери
+        printNumDoors(doorsCount);
 
-        // Задание 1: Добавить ещё 10 дверей к существующим
-        // Задание 2: Убрать 5 дверей
-        // Задание 3: Сделать отдельный метод для вывода в консоль колличества дверей
-        // Задание 4*: Сделать отдельный метод в котором вы добавляете по 1 двери в цикле и выводите на консоль
-        // Пройти по циклу надо 5 раз
-        // Попробовать использовать уже существующие методы в новом
+// Убираем 5 дверей
+        for(int i=0;i<5;i++){
+            doorsCount=decOneDoor(doorsCount);
+        }
+// Выводим двери
+        printNumDoors(doorsCount);
+
+// Двери в цикле (+5)
+        cycleDoors(doorsCount);
+
+
     }
 
-    private static int addOneDoor(int doorsCountOlolo) {
-        doorsCountOlolo = doorsCountOlolo + 1;
-        return doorsCountOlolo;
+    private static int addOneDoor(int addDoor) {
+        addDoor++;
+        return addDoor;
     }
 
-    private static int addTwoDoors(int doorsCountOlolo) {
-        doorsCountOlolo = doorsCountOlolo + 2;
-        return doorsCountOlolo;
+    private static int decOneDoor(int decDoor) {
+        decDoor--;
+        return decDoor;
+    }
+
+    private static void printNumDoors(int numDoors){
+        System.out.println("Current numbers of Doors is: " + numDoors);
+    }
+
+    private static void cycleDoors(int addDoor){
+        for (int i=0;i<5;i++){
+            addDoor = addOneDoor(addDoor);
+            printNumDoors(addDoor);
+        }
     }
 
 }
